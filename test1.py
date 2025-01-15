d43c58ab1f7ae1046406aac454535f1d8f4a2b66
MOONHYUNJUN <ansguswns119@gmail.com>
2025-01-15 03:48:47 +0000

    slack까지 만듦

--- test1.py
+++ test1.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 import os
 from binance.client import Client
+from binance.enums import *    #test 가져오는거
+import slack_sdk   # SLACK 가져오는거
 # .env 파일 로드
 load_dotenv()
 
@@ -16,4 +18,32 @@ secret_key = os.getenv('Secret_Key')
 
 client = Client(api_key, secret_key)
 
+#시장가 거래
+#order = client.order_market_buy(
+#    symbol='BNBBTC',
+#    quantity=100)
+#order = client.order_market_sell(
+#    symbol='BNBBTC',
+#    quantity=100)
 
+
+#TEST 거래
+#order = client.create_test_order(
+#    symbol='BTCUSDT',    #코인이름
+#    side=SIDE_BUY,       #매수,매도
+#    type=ORDER_TYPE_LIMIT,  #시정가 지정가 지정하는거 
+#    timeInForce=TIME_IN_FORCE_GTC,  #제한시간
+#    quantity=0.01,    #살 코인 갯수
+#    price='96784.3')  #갯수
+
+slack_key = os.getenv('Slack_Token')   #slack으로 매수 매도 확인
+slack = slack_sdk.WebClient(token=slack_key)
+
+user_id = "U088PN3C5FC"
+slack_msg = f'<@{user_id}> 주문내역' 
+
+response = slack.chat_postMessage(
+    channel="거래",
+    text=slack_msg
+)
+#print(response)
