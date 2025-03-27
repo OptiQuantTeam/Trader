f9c86e9b65b397050fa6898390a0060da6791386
lucas <wymam1@gmail.com>
2025-03-27 08:26:32 +0000

    dev

--- lambda_function_dev.py
+++ lambda_function_dev.py
@@ -39,10 +39,10 @@ def lambda_handler(event, context):
         client = Client(config['api_key'], config['secret_key'])
         slackBot = SlackBot(config['slack_token'], config['slack_channel'], config['slack_user'])
 
-        slackBot.send_message(resp=f"test1 : {event['mode']}")
-        if event['mode'] == 'dev':
-            slackBot.send_message(resp=f"test2 : {event['mode']}")
-            btc = client.futures_symbol_ticker(symbol="BTCUSDT")
+        #slackBot.send_message(resp=f"test1 : {event['mode']}")
+        #if event['mode'] == 'dev':
+        #    slackBot.send_message(resp=f"test2 : {event['mode']}")
+        #    price = client.futures_symbol_ticker(symbol=event['symbol'])['price']
             
             #event['price'] = float(btc['price'])
             #slackBot.send_message(resp=f"BTC 현재가격 : {event['price']}")
